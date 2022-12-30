b337f5190310c70aced997cb3d8b4ba67dd2d8e1
Arthur Zhang <azhangvo@gmail.com>
2022-12-30 15:31:24 -0500

    opencv setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cv::Mat mat(400, 400, CV_32F);
+
+    cv::circle(mat, cv::Point(200, 200), 150, cv::Scalar(255, 0, 0), -1);
+
+    cv::imshow("Window", mat);
+
+    cv::waitKey(0);
+
+    cv::destroyAllWindows();
+
     return 0;
 }
